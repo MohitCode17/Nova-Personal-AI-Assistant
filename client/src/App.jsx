5b9eb34f939eb12f797ecc00c3527609275faa5f
MohitCode17 <mohit.codes17@gmail.com>
2026-01-16 22:08:50 +0530

    remove comment in app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -136,55 +136,6 @@ const App = () => {
         </div>
       </footer>
     </div>
-    // <div className="bg-neutral-900 text-white h-screen flex flex-col">
-    //   {/* HEADER */}
-    //   <header className="border-b border-neutral-800 px-4 py-3">
-    //     <h1 className="text-xl font-semibold">Nova</h1>
-    //   </header>
-    //   {/* CHAT SECTION */}
-    //   <main className="flex-1 overflow-y-auto px-4 py-6">
-    //     {messages.map((msg, index) => (
-    //       <div
-    //         key={index}
-    //         className={`mb-4 max-w-fit p-4 rounded-2xl text-sm bg-neutral-800 ${
-    //           msg.role === "user" ? "ml-auto" : ""
-    //         }`}
-    //       >
-    //         <div className="prose prose-invert max-w-none">{msg.content}</div>
-    //       </div>
-    //     ))}
-
-    //     {loading && (
-    //       <div className="animate-pulse text-sm text-neutral-400">
-    //         Thinking...
-    //       </div>
-    //     )}
-
-    //     <div ref={chatEndRef} />
-    //   </main>
-
-    //   {/* INPUT */}
-    //   <footer className="border-t border-neutral-800 p-4">
-    //     <div className="flex gap-3">
-    //       <textarea
-    //         rows={"1"}
-    //         value={input}
-    //         onChange={(e) => setInput(e.target.value)}
-    //         onKeyDown={handleKeyDown}
-    //         disabled={loading}
-    //         placeholder="Ask anything..."
-    //         className="flex-1 resize-none bg-neutral-800 p-3 rounded-xl border border-neutral-700"
-    //       />
-    //       <button
-    //         disabled={loading}
-    //         onClick={handleSendMessage}
-    //         className="bg-neutral-800 px-5 py-3 rounded-xl disabled:opacity-40"
-    //       >
-    //         Ask
-    //       </button>
-    //     </div>
-    //   </footer>
-    // </div>
   );
 };
 
